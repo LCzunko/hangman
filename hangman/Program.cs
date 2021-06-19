cf6bf60b9dcba8871ded7cbe26874355781ccc24
LCzunko <lczunkO@gmail.com>
2021-06-19 17:37:58 +0200

    Added functionality for selecting a letter; misc improvements

--- hangman/Program.cs
+++ hangman/Program.cs
@@ -13,6 +13,88 @@ namespace hangman
             Lives lives = new Lives(5);
             lives.curLives = lives.maxLives;
 
+            RenderIntro();
+
+            RenderChoose(lives);
+            int selectGuess = ChooseGuess(lives);
+
+            if (selectGuess == 1)
+            {
+                RenderGuessLetter(lives, selectGuess);
+                char selectLetter = ChooseLetter(lives, selectGuess);
+                Console.WriteLine("The character is " + selectLetter);
+                Console.ReadLine();
+                
+            }
+            else if (selectGuess == 2)
+            {
+                // RenderGuessWord(lives, selectGuess);
+                // string selectWord = ChooseWord(lives, selectGuess);
+                // Console.WriteLine("The word is " + selectWord);
+                Console.WriteLine("The word is unimplemented");
+                Console.ReadLine();
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Successfully selected " + selectGuess);
+
+
+            Console.ReadLine();
+        }
+
+        static void RenderGuessLetter(Lives lives, int selectGuess)
+        {
+            Console.Clear();
+            Console.WriteLine("Lives: " + lives.curLives);
+            Console.WriteLine("");
+            Console.WriteLine("Enter \"1\" to guess a letter, or \"2\" to guess the whole word: " + selectGuess);
+            Console.WriteLine();
+            Console.Write("Guess a letter: ");
+
+            return;
+        }
+
+        static char ChooseLetter(Lives lives, int selectGuess)
+        {
+            char selectLetter ='0';
+
+            for (; ; )
+            {
+                try { selectLetter = Convert.ToChar(Console.ReadLine()); }
+                catch (System.FormatException) { }
+                if (Char.IsLetter(selectLetter)) break;
+                Console.WriteLine("");
+                Console.Write("Invalid selection, press enter to retry.");
+                Console.ReadLine();
+                RenderGuessLetter(lives, selectGuess);
+            }
+
+            selectLetter = Char.ToLower(selectLetter);
+            return selectLetter;
+        }
+
+        static int ChooseGuess(Lives lives)
+        {
+            int selectGuess = 0;
+
+            for (; ; )
+            {
+                try { selectGuess = Convert.ToInt32(Console.ReadLine()); }
+                catch (System.FormatException) { }
+                if (selectGuess == 1 || selectGuess == 2) break;
+                Console.WriteLine("");
+                Console.Write("Invalid selection, press enter to retry.");
+                Console.ReadLine();
+                RenderChoose(lives);
+            }
+
+            return selectGuess;
+        }
+
+
+        static void RenderIntro()
+        {
             Console.WriteLine("Welcome to Hangman!");
             Console.WriteLine("Your goal is to guess the capital of a country.");
             Console.WriteLine("You can guess one letter at a time. Guessing the wrong letter costs 1 life.");
@@ -21,16 +103,10 @@ namespace hangman
             Console.Write("Good luck! Press enter to continue.");
             Console.ReadLine();
 
-            Render(lives);
-            int selectGuess = ChooseGuess(lives);
-
-            Console.WriteLine();
-            Console.WriteLine("Successfully selected " + selectGuess);
-            Console.ReadLine();
+            return;
         }
 
-
-        static void Render(Lives lives)
+        static void RenderChoose(Lives lives)
         {
             Console.Clear();
             Console.WriteLine("Lives: " + lives.curLives);
@@ -38,45 +114,8 @@ namespace hangman
             Console.Write("Enter \"1\" to guess a letter, or \"2\" to guess the whole word: ");
 
             return;
-
         }
 
-        static int ChooseGuess(Lives lives)
-        {
-            int selectGuess = 0;
 
-            try
-            {
-                selectGuess = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (System.FormatException)
-            {
-                Console.WriteLine("");
-                Console.Write("Invalid selection, press enter to retry.");
-                Console.ReadLine();
-                Render(lives);
-                selectGuess = ChooseGuess(lives);
-                return selectGuess;
-            }
-
-            if (selectGuess == 1)
-            {
-                return selectGuess;
-            }
-            else if (selectGuess == 2)
-            {
-                return selectGuess;
-            }
-            else
-            {
-                Console.WriteLine("");
-                Console.Write("Invalid selection, press enter to retry.");
-                Console.ReadLine();
-                Render(lives);
-                selectGuess = ChooseGuess(lives);
-                return selectGuess;
-            }
-
-        }
     }
 }
